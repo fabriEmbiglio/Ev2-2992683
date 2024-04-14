b69825437d8a5bf098d51f77ca0cbf6361de18fc
fabri <fabrizioembiglio@gmail.com>
2024-04-14 16:05:17 -0600

    first commit

---
+++ Ev2-2992683/IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Ev2-2992683
+//
+//  Created by Alumno on 03/04/24.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
+
---
+++ Ev2-2992683/ResultsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ResultViewController.swift
+//  Ev2-2992683
+//
+//  Created by Alumno on 03/04/24.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into:[AnimalType: Int]()) { (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 >
+           $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+    }
+    
+   
+    
+}
+    
+
+
